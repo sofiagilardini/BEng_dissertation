2697578037d51e78a74f7bc7b1281ba458d690a4
sofia <sofia.gilardini@gmail.com>
2024-02-04 21:41:26 +0000

    finished iterations of emg data: cutoff, win_size, win_stride

--- datapipe.py
+++ datapipe.py
@@ -14,6 +14,7 @@ import cebra.models
 import cebra.data
 from cebra.models.model import _OffsetModel, ConvolutionalModelMixin
 from scipy.signal import butter, lfilter
+import os
 #import pickle
 
 
@@ -52,7 +53,6 @@ def emg_process(cutoff_val, size_val, stride_val, user, dataset):
             print("emg windowing at step ", i)
             emg_window_WL = auxf.WL(emg_window, size, stride) 
             print("emg WL extracting at step ", i)
-
             WL = True
             emg_window_LV = auxf.LV(emg_window, size)
             print("emg LV extracting at step ", i)
@@ -70,11 +70,11 @@ def emg_process(cutoff_val, size_val, stride_val, user, dataset):
         # I need to save this in a folder named EMG data, processed 
         
         print("about to save EMG data")
-        np.save(f"./emg_data_processed/emg_{user}_{dataset}_{cutoff}_{win_size}_{win_stride}_{feat_ID}.npy", emg_windows_stacked)
+        np.save(f"./emg_data_processed/emg_{user}_{dataset}_{cutoff_val}_{size_val}_{stride_val}_{feat_ID}.npy", emg_windows_stacked)
         print("saved EMG data")
 
 
-        emg_data_ID = f"{user}_{dataset}_{cutoff}_{win_size}_{win_stride}_{feat_ID}"
+        emg_data_ID = f"{user}_{dataset}_{cutoff_val}_{size_val}_{stride_val}_{feat_ID}"
 
         return emg_data_ID
 
@@ -85,7 +85,6 @@ def glove_process(cutoff_val, size_val, stride_val, user, dataset):
      
     size, stride = auxf.slidingWindowParameters(2000, (size_val*10**(-3)), (stride_val*10**(-3)))
     
-
     emg_u1_d1, glove_u1_d1, stimulus_u1_d1 = auxf.getdata(user = user, dataset = dataset)
     print("got glove data")
 
@@ -106,12 +105,76 @@ def glove_process(cutoff_val, size_val, stride_val, user, dataset):
 
     glove_data_array = np.array(glove_data)
 
-    np.save(f"./glove_data_processed/glove_{user}_{dataset}_{cutoff}_{win_size}_{win_stride}.npy", glove_data_array)
+    np.save(f"./glove_data_processed/glove_{user}_{dataset}_{size_val}_{stride_val}.npy", glove_data_array)
     print("saved glove data")
 
-    glove_data_ID = f"{user}_{dataset}_{cutoff}_{win_size}_{win_stride}"
+    glove_data_ID = f"{user}_{dataset}_{size_val}_{stride_val}"
 
     return glove_data_ID
+
+
+def cebraGridSearch_Multi(emg_data, emg_data_ID, glove_data, glove_data_ID):
+
+
+    models_dir = f"saved_models/{emg_data_ID}_{glove_data_ID}"
+
+    # 1. Define the parameters, either variable or fixed
+    params_grid = dict(
+        model_architecture = ["offset5-model", "offset10-model"], # I need to define my own models here 
+        batch_size = [2**5, 2**6, 2**7],
+        output_dimension = [3, 6, 9],
+        learning_rate = [0.001, 0.0001],
+        time_offsets = [1, 5, 10, 15, 20, 50],
+        max_iterations = 1, #10000,
+        temperature_mode = "auto",
+        verbose = True)
+
+
+    # set fraction for training and for testing 
+
+    training_ratio = 0.4
+    testing_ratio = 0.2
+
+    # not sure if it is 0 or 1
+
+    trainingset = emg_data.shape[0] * training_ratio
+    testingset = emg_data.shape[0] * testing_ratio
+
+
+    glove_channels_list = [1, 4, 6, 8, 11, 14]
+
+
+    # 2. Define the datasets to iterate over
+    datasets = {"dataset1": (emg_data[:trainingset, :], glove_data[:trainingset, :])} # behavioral contrastive learning all glove channels 
+                #"dataset2": (emg_data[:trainingset, :], glove_data[:trainingset, glove_channels_list])} # behavioral contrastive learning selection of channels (zhangcontinuous)
+
+
+    # 3. Create and fit the grid search to your data
+    grid_search = cebra.grid_search.GridSearch()
+
+
+    # fit the model and then save in a folder that is specified according to the emg_data_ID and glove_data_ID -> although I am not sure if the 
+    # find best model function is going to work in that case
+
+    grid_search.fit_models(datasets=datasets, params=params_grid, models_dir= models_dir)
+
+    # 4. Get the results
+    df_results = grid_search.get_df_results(models_dir= models_dir)
+
+    df_results.to_csv("df_models.csv")
+
+    # save dictionary to person_data.pkl file
+
+    # here, instead of getting the best model for saved_models, I could iterate through every sub folder of saved_models and find the best model there ?
+    models, parameter_grid = grid_search.load(dir=models_dir)
+
+
+    # 5. Get the best model for a given dataset
+    best_model, best_model_name = grid_search.get_best_model(dataset_name="dataset1", models_dir=models_dir)
+
+    print(f"name of best model for emg {emg_data_ID} and glove {glove_data_ID}", best_model_name)
+
+    return best_model, best_model_name
 
 
 
@@ -130,84 +193,62 @@ order = 6 # order for LPF
 for cutoff in cutoff_emg:
     for size in win_size:
         for stride in win_stride:
-
             emg_process(cutoff_val=cutoff, size_val=size, stride_val=stride, user = user, dataset = dataset)
-            glove_process(cutoff_val = None, size_val = size, stride_val = stride, user = user, dataset = dataset)
+
+for size in win_size:
+    for stride in win_stride:
+        glove_process(cutoff_val = None, size_val = size, stride_val = stride, user = user, dataset = dataset)
 
 
 
+# def extractIDs(directory):
 
-def cebraGridSearch_Multi(emg_data, emg_data_ID, glove_data, glove_data_ID):
+#     emg_id_list = []
+#     glove_id_list = []
 
-    # 1. Define the parameters, either variable or fixed
-    params_grid = dict(
-        model_architecture = ["offset5-model", "offset10-model"],
-        batch_size = [2**4, 2**5, 2**6, 2**7],
-        output_dimension = [3, 6, 9, 12],
-        learning_rate = [0.005, 0.001, 0.0001],
-        time_offsets = [1, 5,10,15,20, 50],
-        max_iterations = 1, #10000,
-        temperature_mode = "auto",
-        verbose = True)
+#     for filename in os.listdir(directory):
+#         if filename.endswith('.npy'):
 
+#             # if emg data
+#             if filename.startswith('emg'):
+#                 parts = filename.replace('.npy', "").split("_")
+#                 type_data, user, dataset, cutoff_val, size_val, stride_val, feat_ID = parts[0], parts[1], parts[2], parts[3], parts[4], parts[5], parts[6]
+#                 emg_data_ID = f"{user}_{dataset}_{cutoff_val}_{size_val}_{stride_val}_{feat_ID}"
+#                 emg_id_list.append(emg_data_ID)
 
+#                 glove_id_list = None
+            
 
-    # set fraction for training and for testing 
+#             # if glove data
+#             if filename.startswith('glove'):
+#                 parts = filename.replace('.npy', "").split("_")
+#                 type_data, user, dataset, size_val, stride_val = parts[0], parts[1], parts[2], parts[3], parts[4]
+#                 glove_data_ID = f"{user}_{dataset}_{size_val}_{stride_val}"
+#                 glove_id_list.append(glove_data_ID)
 
-    training_ratio = 0.5
-    testing_ratio = 0.3
-
-    # not sure if it is 0 or 1
-
-    trainingset = emg_data.shape[0] * training_ratio
-    testingset = emg_data.shape[0] * testing_ratio
-
-
-
-    # 2. Define the datasets to iterate over
-    datasets = {"dataset2": (emg_data[:trainingset, :], glove_data[:trainingset, :])} # behavioral contrastive learning
-
-
-    # 3. Create and fit the grid search to your data
-    grid_search = cebra.grid_search.GridSearch()
+#                 emg_id_list = None
+    
+#     if emg_id_list == None:
+#         return glove_id_list
+    
+#     elif glove_id_list == None:
+#         return emg_id_list
 
 
-    # fit the model and then save in a folder that is specified according to the emg_data_ID and glove_data_ID -> although I am not sure if the 
-    # find best model function is going to work in that case
+# def findBestModel(emg_ID, glove_ID, directory):
 
-    grid_search.fit_models(datasets=datasets, params=params_grid, models_dir= f"saved_models/{emg_data_ID}_{glove_data_ID}")
+#     emg_data_in = f"./emg_data_processed/emg_{emg_ID}.npy"
+#     glove_data_in = f"glove_data_processed/glove_{gloveID}.npy"
 
-    # 4. Get the results
-    df_results = grid_search.get_df_results(models_dir= f"saved_models/{emg_data_ID}_{glove_data_ID}")
-
-    df_results.to_csv("df_models.csv")
-
-    # save dictionary to person_data.pkl file
-
-    # here, instead of getting the best model for saved_models, I could iterate through every sub folder of saved_models and find the best model there ?
-    models, parameter_grid = grid_search.load(dir=f"saved_models/{emg_data_ID}_{glove_data_ID}")
+#     # cebraGridSearch_Multi returns best_model, best_model_name
 
 
-    # 5. Get the best model for a given dataset
-    best_model, best_model_name = grid_search.get_best_model(dataset_name="dataset2", models_dir="saved_models")
-
-    print(f"name of best model for emg {emg_data_ID} and glove {glove_data_ID}", best_model_name)
-
-
-
+#     best_model, best_model_name = cebraGridSearch_Multi(emg_data=emg_data_in,
+#                           glove_data=glove_data_in,
+#                           emg_data_ID=emg_ID,
+#                           glove_data_ID=glove_ID)
+    
 
 
-# # I want a function that takes in my desired channels for glove data
-# # and returns a list of lists with only those channels ? do I ? 
-
-# # 1. Define the parameters, either variable or fixed
-# params_grid = dict(
-#     model_architecture = "offset5-model",
-#     batch_size = [2**4, 2**5, 2**6, 2**7],
-#     output_dimension = [3, 6, 9, 12],
-#     learning_rate = [0.001, 0.005, 0.0001],
-#     time_offsets = [1,2,5,10,15,20],
-#     max_iterations = 10000,
-#     temperature_mode = "auto",
-#     verbose = True)
+#     # give the best model an ID and then save it in the folder best_models
 
